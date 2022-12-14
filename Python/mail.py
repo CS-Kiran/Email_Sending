126364a6c1347bb8cf3de18bcfc6c91b3075ee28
Code@Kiran <109469540+CS-Kiran@users.noreply.github.com>
2022-12-14 20:36:39 +0530

    Update mail.py

--- Python/mail.py
+++ Python/mail.py
@@ -1,12 +1,17 @@
-import emails
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587)
+
 s.starttls()
 #(220, b'2.0.0 Ready to start TLS')
-s.login("kiran2002bp@gmail.com", "uqatmwfbwhbzoxly")
+
+s.login("sender@gmail.com", "senderspassword")
 #(235, b'2.7.0 Accepted')
+
 message = "Sending Email 3 using Python Successfully"
-s.sendmail("kiran2002bp@gmail.com", "kiran1007patil@gmail.com", message)
+s.sendmail("sender@gmail.com", "receiver@gmail.com", message)
 #{}
+
 s.quit()
 #(221, b'2.0.0 closing connection n9-20020a17090a670900b002190eabb890sm6357870pjj.25 - gsmtp')
+
+#OUTPUTS Expected
